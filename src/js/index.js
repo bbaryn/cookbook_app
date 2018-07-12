33d8ca1445e5f3a5db8519cbfcd6cb81d3838cd5
Bartek Baryn <marlin34@gmail.com>
2018-07-12 06:55:30 +0200

    Recipe view has been added

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView'
+import * as recipeView from './views/recipeView'
 import {
     elements,
     renderLoader,
@@ -69,7 +70,9 @@ const ctrlRecipe = async () => {
 
     if (id) {
         //Prepare UI
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+        
         //Create new recipe object
         state.recipe = new Recipe(id);
         
@@ -83,7 +86,8 @@ const ctrlRecipe = async () => {
             state.recipe.calcServings();
 
             //Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (error) {
             console.log(error);
         }
